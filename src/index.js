af884a881301052256a57c6caf74a2f65213d47a
kushal <kushal.maurya@acsicorp.com>
2024-04-09 17:23:12 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({
@@ -5,3 +6,9 @@ dotenv.config({
 });
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log("Server started at port " + (process.env.PORT || 8000));
+        })
+        
+    }).catch((err) => console.log("MongoDb connection error ::", err));
