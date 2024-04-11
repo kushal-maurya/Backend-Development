3b035dc2c10cba03b52b307e37928f950452cc70
kushal <kushal.maurya@acsicorp.com>
2024-04-11 15:16:01 +0530

    Created register user controller and route

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,10 +1,16 @@
 class ApiError extends Error {
-    constructor(statusCode, message = "Something went wrongggg", errors = [], stack = "") {
+    constructor(
+        statusCode, 
+        message = "Something went wrongggg", 
+        errors = [], 
+        stack = ""
+    ){
         super(message);
         this.errors = errors;
         this.statusCode = statusCode;
         this.success = false;
         this.data = null;
+        
         if (stack) this.stack = stack;
         else Error.captureStackTrace(this, this.constructor);
     }
